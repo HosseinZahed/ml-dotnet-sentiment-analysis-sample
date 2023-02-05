00d131337a5e388f1467dfaebf6f0f2583624bf2
Hossein Zahed <hzahed@outlook.com>
2023-02-05 02:04:47 +0100

    Project added

---
+++ SentimentAnalysisML/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SentimentAnalysisML;
+
+Console.WriteLine("-- Sentiment Analysis with ML.NET --");
+
+//Load sample data
+var sampleData = new SentimentModel.ModelInput()
+{
+    Text = @"@VirginAmerica 2-hour delay is not acceptable!",
+    //Text = @"@VirginAmerica that was a great offer for Christmas holidays",
+    //Text = @"@VirginAmerica nothing competitive from the other options",
+};
+
+//Load model and predict output
+var mupltipleOutputs = SentimentModel.PredictAllLabels(sampleData);
+
+Console.WriteLine($"Text: {sampleData.Text}");
+foreach (var item in mupltipleOutputs)
+{    
+    Console.WriteLine($"Label: {item.Key} -> Score: {item.Value}");
+}
+
